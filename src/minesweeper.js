610819312aec21272bddf57377603d97235dcc24
Erick Chavarria-Romo <echavarria-romo@atlasproject.net>
2017-08-15 20:05:42 -0400

    Second part of the project: makes the application more dynamic

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,17 +1,20 @@
-const blankLine = "  |   |  ";
+const printBoard = board => {
+ console.log('Current Board: ');
+ console.log(board[0].join(' | '));
+ console.log(board[1].join(' | '));
+ console.log(board[2].join(' | '));
 
-console.log("This is what an empty board would look like:"); 
+}
 
-console.log(blankLine); 
-console.log(blankLine); 
-console.log(blankLine); 
+const board = [[' ', ' ', ' '], 
+			  [' ', ' ', ' '], 
+			  [' ', ' ', ' ']
+]; 
 
-const guessLine = '1 |   |  ';
+printBoard(board)
 
-const bombLine = '  | B |  ';
+board[0][1] = 1; 
 
-console.log('This is what a board with a guess and a bomb on it would look like: ')
+board[2][2] = 'B'; 
 
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+printBoard(board)
